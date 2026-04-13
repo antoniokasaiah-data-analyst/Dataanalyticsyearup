3912a9364edc76df7d079b4ff14195a51e9d8db8
kasaiah antonio <kantonio@my.yearupunited.org>
2026-04-13 07:07:03 -0700

    update sql file

--- week-2/Day_1/Ex1B_first_query.sql
+++ week-2/Day_1/Ex1B_first_query.sql
@@ -1,3 +1,12 @@
 /* 
 Sakila is a database used to collect information ________ and _______ for use by _______ .
 */
+
+USE sakila;
+Select * FROM actor;
+SELECT count(*) FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film;
+SELECT count(*) FROM film; -- Retrieved 1000 records
+
+
