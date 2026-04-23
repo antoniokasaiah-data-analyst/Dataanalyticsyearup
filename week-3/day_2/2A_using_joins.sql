02a61598a863ecdee63891142961b3a12c68b467
kasaiah antonio <kantonio@my.yearupunited.org>
2026-04-23 08:26:27 -0700

    2a

--- week-3/day_2/2A_using_joins.sql
+++ week-3/day_2/2A_using_joins.sql
@@ -52,18 +52,74 @@ JOIN categories AS t3
 ON t1.CategoryID = t3.CategoryID
 ORDER BY productname;
 
-4. Create a single query to list the order id, ship name, ship address, and shipping 
+/*4. Create a single query to list the order id, ship name, ship address, and shipping 
 company name of every order that shipped to Germany. Assign the shipping company 
 name the alias ‘Shipper.’ Order by the name of the shipper, then the name of who it 
-shipped to.
-5. Start from the same query as above (#4), but omit OrderID and add logic to group by 
-ship name, with a count of how many orders were shipped for that ship name.
-6. Create a single query to list the order id, order date, ship name, ship address of all 
+shipped to. */
+
+USE northwind;
+
+SELECT *
+FROM information_schema.KEY_COLUMN_USAGE
+WHERE TABLE_NAME = 'orders';
+
+SELECT * 
+FROM shippers;
+
+SELECT * 
+FROM orders;
+
+SELECT t1.orderid,t1.shipname,t1.shipaddress,t2.companyname AS Shipper
+FROM orders AS t1
+JOIN shippers AS t2
+ON t1.shipvia = t2.shipperid
+WHERE t1.shipcountry = 'germany'
+ORDER BY shipper, shipaddress;
+
+
+/* 5. Start from the same query as above (#4), but omit OrderID and add logic to group by 
+ship name, with a count of how many orders were shipped for that ship name. */
+
+SELECT count(t1.shipname),t1.shipname
+FROM orders AS t1
+JOIN shippers AS t2
+ON t1.shipvia = t2.shipperid
+WHERE t1.shipcountry = 'germany'
+GROUP BY t1.shipname
+ORDER BY t1.shipname;
+
+
+
+
+/* 6. Create a single query to list the order id, order date, ship name, ship address of all 
 orders that included Sasquatch Ale.
 ∗ Hint: You will need to join on three tables to accomplish this. (One of these tables 
 has a sneaky space in the name, so you will need to surround it with backticks, like 
-this: `table name`)
-7. Save your changes to 2A_using_joins.sql and use Git Bash to add, commit, and push 
-to DataAnalytics/week-03. */
+this: `table name`) */
+
+SELECT t1.orderid,t1.shipname,t1.shipaddress,t1.orderdate,t1.shipaddress
+FROM orders AS t1
+JOIN shippers AS t2
+ON t1.shipvia = t2.shipperid
+WHERE t1.shipaddress LIKE '%ALE%'
+ORDER BY shipaddress;
+
+-- 6. List order id, order date, ship name, ship address of all orders that
+-- --    included Sasquatch Ale.
+-- Note: 'order details' has a space in the name so it requires backticks.
+
+SELECT o.OrderID, o.OrderDate, o.ShipName, o.ShipAddress
+FROM orders o
+INNER JOIN `order details` od ON o.OrderID = od.OrderID
+INNER JOIN products p ON od.ProductID = p.ProductID
+WHERE p.ProductName = 'Sasquatch Ale'
+ORDER BY o.OrderID ASC;
+
+
+
+
+
+-- 7. Save your changes to 2A_using_joins.sql and use Git Bash to add, commit, and push 
+-- to DataAnalytics/week-03. */
 
 
