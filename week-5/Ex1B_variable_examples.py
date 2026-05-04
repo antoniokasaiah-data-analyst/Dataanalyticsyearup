2a73ea40e9cf1d82809edac80b945753d526c8e1
Antonio <antoniokasaiah@gmail.com>
2026-05-04 10:53:36 -0700

    update file

---
+++ week-5/Ex1B_variable_examples.py
@@ -0,0 +1,29 @@
+cid = 'Customer ID'
+cname = 'Customers name'
+cgender = 'Customer gender'
+cdob = 'Customer date of birth'
+dln = 'Drivers license number'
+apn= 'Auto policy number'# my variables are abbreviated versions of the string to make them memorable 
+
+ka = 'Kasaiah Antonio'
+city = 'Fort Lauderdale'
+state = 'Florida'
+
+
+# a) What is the full list of reserved words that can’t be used for variable names?
+#False	def	if	raise
+# None	del	import	return
+#True	elif	in	try
+#and	else	is	while
+#as	except	lambda	with
+#assert	finally	nonlocal	yield
+#break	for	not
+#class	form	or
+#continue	global	pass
+
+# b)
+# def - def function is used to define a function or a method in Python.
+# none - There is no null value in Python. The None is an object that represents the absence of a value. It is like an empty object.
+# del The del statement is used to delete an object in Python.
+# import This statement is used to import modules to the project
+# return This keyword is used to exit a function or a method and return some value.
