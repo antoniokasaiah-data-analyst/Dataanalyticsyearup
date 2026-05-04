a7a18fd7083df6b9b99605ead52299d7a8988dc3
Antonio <antoniokasaiah@gmail.com>
2026-05-04 10:13:41 -0700

    new update

--- week-5/Ex1A_hello.py
+++ week-5/Ex1A_hello.py
@@ -1,2 +1,25 @@
 print('Hello World')
 print('hi my name is')
+
+message = 'Hello World '
+print(message)
+
+# The message prints twice because there are to print commands printing code.
+
+# Display dollars and cents
+
+d = 3
+c = .50
+
+print(d + c)
+
+# Results outputed the first to prints and combined 3 and .5 in the finally print
+
+c = c + .25
+print(d + c)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+
+print(d_str +c_str)
+print(d_str +" "+c_str)
